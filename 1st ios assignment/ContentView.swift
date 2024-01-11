3624d9058b621ddd0a68476af2f9ac0ef88626b3
Abhilashreddyg1 <gangadasuabhilashreddy@gmail.com>
2024-01-10 19:56:27 -0800

    Initial Commit

---
+++ 1st ios assignment/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  1st ios assignment
+//
+//  Created by user240435 on 1/10/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("ENJOYING CANADA VIBES")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ 1st ios assignment/_st_ios_assignmentApp.swift
@@ -0,0 +1,17 @@
+//
+//  _st_ios_assignmentApp.swift
+//  1st ios assignment
+//
+//  Created by user240435 on 1/10/24.
+//
+
+import SwiftUI
+
+@main
+struct _st_ios_assignmentApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
